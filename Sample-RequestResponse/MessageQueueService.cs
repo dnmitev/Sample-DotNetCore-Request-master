ea56c32d80adf22144540e154121545012484f6e
Dimitar Mitev <dimitar.mitev@experian.com>
2020-06-29 09:55:22 +0300

    Add project files.

---
+++ Sample-RequestResponse/MessageQueueService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using GreenPipes;
+
+using MassTransit;
+using MassTransit.ActiveMqTransport;
+
+using MessageContracts;
+
+using Microsoft.Extensions.Hosting;
+
+namespace Sample_RequestResponse
+{
+    public class MessageQueueService : BackgroundService
+    {
+        private readonly IBusControl _bus;
+
+        public MessageQueueService()
+        {
+            _bus = Bus.Factory.CreateUsingActiveMq(cfg =>
+                          {
+                              cfg.Host("broker-amq-amqp-dnmitev.apps.appcanvas.net", h =>
+                              {
+                                  h.Username("admin");
+                                  h.Password("admin");
+                              });
+
+                              cfg.ReceiveEndpoint("order-service", ep =>
+                              {
+                                  ep.Handler<SubmitOrder>(context => context.RespondAsync<OrderAccepted>(new
+                                  {
+                                      context.Message.OrderId
+                                  }));
+                                  //ec.Consumer<CreateOBOLInstanceCommandHandler>(provider);
+                              });
+                          });
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            return _bus.StartAsync();
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.WhenAll(base.StopAsync(cancellationToken), _bus.StopAsync());
+        }
+    }
+}
---
+++ Sample-RequestResponse/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Threading.Tasks;
+
+namespace Sample_RequestResponse
+{
+  class Program
+  {
+    static async Task Main(string[] args)
+    {
+      var builder = new HostBuilder()
+        .ConfigureServices((hostContext, services) =>
+        {
+          services.AddHostedService<MessageQueueService>();
+        });
+
+      await builder
+        .RunConsoleAsync();
+    }
+  }
+}
